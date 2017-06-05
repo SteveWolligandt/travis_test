661c4e6ff5cc4bd3b7120006128c78687aba4bc1
Steve Wolligandt <stevewolligandt@gmail.com>
2017-06-05 18:25:36 +0200

    -

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 #include "TestClass.h"
 
 int main() {
+  const int i = 23;
+  if (i == 42)
+    std::cout << "the answer to all\n";
+
   TestClass<double> t(1.0);
   std::cout << t.val() << '\n';
   t.val() = 2.0;
